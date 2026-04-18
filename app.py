95abd64d95c6cdb9f9c4c2ce45ba62deb437be88
Manakov Andrey <manakoooov@gmail.com>
2026-04-18 22:54:22 +0200

    .

--- app.py
+++ app.py
@@ -9,6 +9,11 @@ from email_validator import validate_email, EmailNotValidError
 load_dotenv()
 app = Flask(__name__)
 
+@app.route('/robots.txt')
+@app.route('/sitemap.xml')
+def static_from_root():
+    return send_from_directory(app.root_path, request.path[1:])
+
 @app.route('/')
 def index():
     return render_template('index.html')
